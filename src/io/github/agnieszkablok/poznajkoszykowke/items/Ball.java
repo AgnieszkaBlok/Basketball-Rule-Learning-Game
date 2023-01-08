51a24ab029b2e629edd4cdb747db18b79e2ed706
Piotr <piotrklopotowski@outlook.com>
2023-01-08 16:02:24 +0100

    Add Ball and Whistle classes

---
+++ src/io/github/agnieszkablok/poznajkoszykowke/items/Ball.java
@@ -0,0 +1,15 @@
+package io.github.agnieszkablok.poznajkoszykowke.items;
+
+import javax.imageio.ImageIO;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class Ball extends Item{
+    public Ball(){
+        try {
+            image = ImageIO.read(new FileInputStream("res/objects/ball.png"));
+        } catch (IOException e) {
+            System.out.println("Couldn't read the ball image!");
+        }
+    }
+}
